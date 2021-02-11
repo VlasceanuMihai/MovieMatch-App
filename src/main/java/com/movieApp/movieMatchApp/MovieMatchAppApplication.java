79f028f91052134286e2e19fe60bcacea206bb5c
VlasceanuMihai <mihai.vlasceanu1996@gmail.com>
2021-02-11 19:30:18 +0200

    Add Spring Boot and dependencies

---
+++ src/main/java/com/movieApp/movieMatchApp/MovieMatchAppApplication.java
@@ -0,0 +1,13 @@
+package com.movieApp.movieMatchApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieMatchAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieMatchAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/movieApp/movieMatchApp/MovieMatchAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movieApp.movieMatchApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieMatchAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
