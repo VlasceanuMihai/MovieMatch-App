24c2859c8cc019970a952b5c738a86d2f52d9dd7
VlasceanuMihai <mihai.vlasceanu1996@gmail.com>
2021-02-20 16:52:09 +0200

    Fix CrossOrigin

--- src/main/java/com/movieApp/movieMatchApp/security/WebSecurityConfig.java
+++ src/main/java/com/movieApp/movieMatchApp/security/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import com.movieApp.movieMatchApp.services.UserAuthService;
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,6 +13,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@Profile("!https")
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final UserAuthService userAuthService;
@@ -28,6 +30,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) {
         http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers(HttpMethod.OPTIONS, "/api/v1/registration*").permitAll();
+                .antMatchers(HttpMethod.OPTIONS, "/api/v1/registration*").permitAll()
+                .antMatchers(HttpMethod.GET, "/index*", "/static/**", "/*.js", "/*.json", "/*.ico").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/index.html")
+                .loginProcessingUrl("/performLogin")
+                .defaultSuccessUrl("/homepage.html", true).failureUrl("/index.html?error=true");
     }
+
 }
