6be18f98c38b77d95b03626610d857b2ad9d21b1
mateistefan13 <r.mateistefan@gmail.com>
2021-02-22 19:38:52 +0200

    Added a junction table entity

---
+++ src/main/java/com/movieApp/movieMatchApp/models/UserAndMovie.java
@@ -0,0 +1,31 @@
+package com.movieApp.movieMatchApp.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "user_and_movie")
+public class UserAndMovie {
+
+    @EmbeddedId
+    UserAndMovieKey id;
+
+    @ManyToOne
+    @MapsId("userId")
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @MapsId("movieId")
+    @JoinColumn(name = "movie_id")
+    private Movie movie;
+
+}
